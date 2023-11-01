970c6a7e7a82a40b39177d2d7339202262b4ad22
Imre Nemeth <imi.nemeth@gmail.com>
2023-11-01 11:09:42 +0100

    Add markdown plugin

--- lua/imi/plugins-setup.lua
+++ lua/imi/plugins-setup.lua
@@ -104,6 +104,15 @@ return packer.startup(function(use)
 
 	use("karb94/neoscroll.nvim")
 
+       use({
+               "iamcco/markdown-preview.nvim",
+               run = "cd app && npm install",
+               setup = function()
+                       vim.g.mkdp_filetypes = { "markdown" }
+               end,
+               ft = { "markdown" },
+       })
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
