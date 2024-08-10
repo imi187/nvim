2b1a3f48e2061d1b942f905213685cb9a0a40771
Imre Nemethµ <imi.nemeth@gmail.com>
2024-08-10 20:27:09 +0200

    New lock file and update scheme colors

--- lua/imi/plugins/colorscheme.lua
+++ lua/imi/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-  --"folke/tokyonight.nvim",
-  "Mofiqul/vscode.nvim",
-  priority = 1000,
-  config = function()
-    --[[local bg = "#011628"
+	--"folke/tokyonight.nvim",
+	"Mofiqul/vscode.nvim",
+	priority = 1000,
+	config = function()
+		--[[local bg = "#011628"
     local bg_dark = "#011423"
     local bg_highlight = "#143652"
     local bg_search = "#0A64AC"
@@ -12,10 +12,10 @@ return {
     local fg_dark = "#B4D0E9"
     local fg_gutter = "#627E97"
     local border = "#547998"]]
-    --
-
-    --require("vscode.colors").setup()
-    --[[{
+		--
+		--
+		--require("vscode.colors").setup()
+		--[[{
       style = "night",
       on_colors = function(colors)
         colors.bg = bg
@@ -35,8 +35,38 @@ return {
         colors.fg_sidebar = fg_dark
       end
     })]]
-    --
+		--
+		--
+		local c = require("vscode.colors").get_colors()
+		require("vscode").setup({
+			-- Alternatively set style in setup
+			-- style = 'light'
 
-    vim.cmd("colorscheme vscode")
-  end,
+			-- Enable transparent background
+			transparent = true,
+
+			-- Enable italic comment
+			italic_comments = true,
+
+			-- Underline `@markup.link.*` variants
+			underline_links = true,
+
+			-- Disable nvim-tree background color
+			disable_nvimtree_bg = true,
+
+			-- Override colors (see ./lua/vscode/colors.lua)
+			color_overrides = {
+				vscLineNumber = "#FFFFFF",
+			},
+
+			-- Override highlight groups (see ./lua/vscode/theme.lua)
+			group_overrides = {
+				-- this supports the same val table as vim.api.nvim_set_hl
+				-- use colors from this colorscheme by requiring vscode.colors!
+				Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+			},
+		})
+
+		vim.cmd("colorscheme vscode")
+	end,
 }
