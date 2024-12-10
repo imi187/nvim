e9cdf93bf531dee834e6cfac73ba1d9e8686616a
Imre Németh <imi@Imres-MacBook-Air.local>
2024-12-10 15:51:35 +0100

    Will fix shotcut Close tab and Open Tab for barbar

--- lua/imi/core/keymaps.lua
+++ lua/imi/core/keymaps.lua
@@ -4,21 +4,25 @@ local keymap = vim.keymap -- force conciseness
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights"})
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights" })
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Descrement number" })
 
 -- windows management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically"})
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally"})
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split equal size"})
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split"})
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
+keymap.set("n", "<leader>tx", "<cmd>BufferClose<CR>", { desc = "Close current tb" })
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab"})
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab"})
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab"})
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab"})
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab"})
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+-- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>BufferNext<CR>", { desc = "Go to next tab" })
+-- keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>BufferPrevious<CR>", { desc = "Go to previous tab" })
+
+---keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
