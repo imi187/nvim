1aadc1cf9263c96b7586f5512498817c6ecdbd0e
Imre Németh <imi.nemeth@gmail.com>
2025-03-29 22:06:01 +0100

    This will add multi select with ctrl+n

--- lua/imi/plugins/init.lua
+++ lua/imi/plugins/init.lua
@@ -2,4 +2,5 @@ return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigator
   "github/copilot.vim",
+  "mg979/vim-visual-multi",
 }
